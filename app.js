4ec72b754615bafe22b0701fadaaa9db88659692
Arefi6 <arefinhossain3@gmail.com>
2020-07-24 11:10:12 +0600

    Deposit Section Added

--- app.js
+++ app.js
@@ -8,5 +8,23 @@ loginBtn.addEventListener("click",function(){
     transitionArea.style.display = "block";
 
 });
+const depositBtn = document.getElementById("deposit-btn");
+depositBtn.addEventListener("click",function(){
+   const depositAmount = document.getElementById("deposit-amount").value;const depositNumber = parseFloat(depositAmount);
+
+    updateSpan("current-deposit",depositNumber);
+    updateSpan("current-balance",depositNumber);
+    document.getElementById("deposit-amount").value="";
+
+   //console.log(totalBalance);
+function updateSpan(id,depositNumber){
+
+    const currentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber = parseFloat(currentBalance); 
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document.getElementById(id).innerText = totalBalance;
+      }   
+});
+const withdrawBtn = document.getElementById("withdraw-btn");   
 
 
